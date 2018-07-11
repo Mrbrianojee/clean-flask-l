4a5415086ded88ea1f131b2f49e6d5153fc5f16b
Brianojee <brian@codeinstitute.net>
2018-07-11 17:19:13 +0000

    added templates and navigation

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello world!!'
+def index():
+    return render_template('index.html', title='Home')
+    
+@app.route('/about')   
+def about():
+   return render_template('index.html', title='About')
+    
+@app.route('/portfolio')   
+def portfolio():
+    return render_template('index.html', title='Portfolio')
+    
+@app.route('/contact')   
+def contact():
+    return render_template('index.html', title='Contact')
     
 if __name__ == '__main__':
     app.run(host = os.environ.get("IP"),
